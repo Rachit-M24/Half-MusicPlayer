b0816903224375d1690a773a9a3c823b62157687
Rachit Mahida <127675261+Rachit-M24@users.noreply.github.com>
2023-08-21 18:23:02 +0530

    Add files via upload

---
+++ projectMusicPlayer.js
@@ -0,0 +1,9 @@
+let Isongs =document.getElementById("ISongs");
+let IsongsList =document.getElementById("InternationalSongs");
+
+// Isongs.addEventListener("click",function(){
+//  document.getElementById("InternationalSongs").style.visibility ="visble";
+// })
+function Show(){
+   document.getElementById("InternationalSongs").style.visibility ="visble";
+}
